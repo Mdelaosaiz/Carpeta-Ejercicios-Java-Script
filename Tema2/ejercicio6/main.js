0ad5eede6a1e5f2d61ea400028dbd3b64b333d03
Marian de la Osa <mariandelaosa@gmail.com>
2020-10-15 18:30:50 +0200

    added new things in ejercicio6

--- Tema2/ejercicio6/main.js
+++ Tema2/ejercicio6/main.js
@@ -1,7 +1,10 @@
 'use strict'
-
+ 
 
 const partner = 'Paca';
 const salute = 'Hola';
 
 console.log(salute + ' ' + partner + ' ' + "encantada de conocerte");
+
+//de forma abreviada 
+//console.log(`hola ${partner} encantada de conocerte`);
