31cf84ca2453f0aa959ba1b3fbaddd7a67b8be6e
Marian de la Osa <mariandelaosa@gmail.com>
2020-10-15 18:14:26 +0200

    ejercicio3

---
+++ Tema2/ejercicio4/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let bill = 128;
+let people = 9;
+
+const payment = bill/people;
+const paymentAna = (bill/people) + 2;
+console.log('Todas tenemos que pagar ' + payment + '€ cada una, menos Ana, que tiene que pagar ' + paymentAna + '€, por ser una borracha');
