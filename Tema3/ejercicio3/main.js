6a8badd7fc4faee125b4232c73ef9cbe3641ba70
Marian de la Osa <mariandelaosa@gmail.com>
2020-10-19 17:14:47 +0200

    ejercicio 3 tema 3

--- Tema3/ejercicio3/main.js
+++ Tema3/ejercicio3/main.js
@@ -1,1 +1,10 @@
 'use strict'
+// Prepara una variable cuyo valor será un número. Escribe las condiciones para el siguiente ejercicio y utilizando la variable haz pruebas para ver que estas se cumplen.
+
+let number = 8;
+
+if (number >=10){
+  console.log(`${number} es mayor que 10`);
+}else if (number <=10){
+  console.log(`${number} es menor que 10`);
+}
