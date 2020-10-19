2e63cbc887b081ff2ee678a79bec51fcb2133694
Marian de la Osa <mariandelaosa@gmail.com>
2020-10-19 17:30:47 +0200

    ejercicio 4 tema 3

--- Tema3/ejercicio4/main.js
+++ Tema3/ejercicio4/main.js
@@ -1,1 +1,21 @@
 'use strict'
+//El primer año de un perro equivale a 15 años de humano
+// El segundo año de un perro equivale a nueve años de humano
+// A partir del tercero, cada año de perro equivale a 5 años de humano.
+// Nota: Prueba que el código funciona correctamente con distintos años (1, 2, 12...).
+
+let dogYears = 8;
+
+let bebyYear = 15;
+let puppyYear = bebyYear + 9;
+let adultYear = puppyYear + (dogYears * 5);
+
+if (dogYears <=0){
+  console.log(`tu perro tiene el equivalente a menos de ${bebyYear} años humanos`);
+} else if (dogYears === 1){
+  console.log(`tu perro tiene el equivalente a ${bebyYear} años humanos`);
+} else if (dogYears === 2){
+  console.log(`tu perro tiene el equivalente a ${puppyYear} años humanos`);
+} else if ( dogYears >=3){
+  console.log(`tu perro tiene el equivalente a ${adultYear} años humanos`);
+}
---
+++ Tema3/ejercicio4/readme.md
@@ -0,0 +1,6 @@
+### Conversor de edad de perro a humano
+Te habrá pasado varias veces de ir por la calle y que alguien te pregunte "perdona, tienes a mano un conversor de edad de perros a humanos" y tener que contestar con vergüenza que no y que esa persona te mire raro... ¡hasta ahora! Vamos a crearla para evitar esto que pasa tan a menudo. Para ello, las reglas son las siguientes:
+El primer año de un perro equivale a 15 años de humano
+El segundo año de un perro equivale a nueve años de humano
+A partir del tercero, cada año de perro equivale a 5 años de humano.
+Nota: Prueba que el código funciona correctamente con distintos años (1, 2, 12...). Imagina el alcance de los daños si la próxima vez que te pare una persona para preguntarte por el conversor... ¡no funciona correctamente!
