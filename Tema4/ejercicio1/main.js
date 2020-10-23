3f779ade8176817b622a6068eef03a25ff04b246
Marian de la Osa <mariandelaosa@gmail.com>
2020-10-23 13:54:55 +0200

    ejercicio 3

--- Tema4/ejercicio1/main.js
+++ Tema4/ejercicio1/main.js
@@ -1,1 +1,7 @@
 'use strict'
+
+function multi(a,b){
+  return a*b;
+}
+
+console.log(multi(5,12));
--- Tema4/ejercicio2/main.js
+++ Tema4/ejercicio2/main.js
@@ -1,1 +1,8 @@
 'use strict'
+
+
+function media(a,b,c,d){
+  return (a+b+c+d)/media.length;
+}
+
+console.log(media(3,6,34,70));
