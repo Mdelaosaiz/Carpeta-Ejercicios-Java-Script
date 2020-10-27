3b2d7b8ea1a480f2a5c8549b7e142c212262d373
Marian de la Osa <mariandelaosa@gmail.com>
2020-10-27 11:11:49 +0100

    ejercicio9

--- Tema4/ejercicio9/main.js
+++ Tema4/ejercicio9/main.js
@@ -1,1 +1,20 @@
 'use strict'
+
+
+function box (isBorder, width, padding, size){
+
+  let result = 0;
+
+  if (isBorder){
+
+    result = width;
+
+  }else { result = width + padding + size;} 
+
+  return "El ancho del contenido es: " + width + "px y el ancho total de la caja es:" + result + "px."
+}
+
+//ejecuta la función recogiendo el resultado en una variable e imprímela en la consola.
+
+console.log(box(true, 100, 5, 200));// no calcula el total porque al ser borderbox, tiene el tamaño que el programador haya dicho... 
+console.log(box(false, 100, 5, 200)); // al no ser Borderbox, se tienen que sumar todas las medidas para obtener el tamaño real de la caja.
